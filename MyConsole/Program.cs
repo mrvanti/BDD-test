e205791b7c2fa6782ae2bcc88fd60ff515f6290d
Martin Jansson <martin.jansson@cgi.com>
2024-12-03 09:14:09 +0100

    Lägger till kod för "Rövarspråket"

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Välkommen hit!");
+while (true)
+{
+    Console.WriteLine("Vad heter du?");
+
+    var name = Console.ReadLine();
+
+
+    var rövarspråket = "";
+    foreach (char c in name ?? "")
+    {
+        if (IsVowel(c))
+        {
+            rövarspråket += c;
+        }
+        else
+        {
+            rövarspråket = rövarspråket + c + "o" + c;
+        }
+    }
+
+    Console.WriteLine("Ditt namn på rövarspråket:");
+    Console.WriteLine(rövarspråket);
+
+
+
+    Console.WriteLine("Avsluta eller köra igen? 'Esc' för att avsluta applikationen, valfri tangent för att mata in igen.");
+
+    // Wait for a key press
+    var key = Console.ReadKey(intercept: true);
+
+    // Check if the key pressed is 'Enter'
+    if (key.Key == ConsoleKey.Escape)
+    {
+        Console.WriteLine("\nAvslutar");
+        break; // Exit the loop
+    }
+
+}
+
+static bool IsVowel(char c)
+{
+    // Convert the character to lowercase for simplicity
+    char lowerChar = char.ToLower(c);
+
+    // Check if the character is in the set of vowels
+    return "aeiouåäö".IndexOf(lowerChar) >= 0;
+}
